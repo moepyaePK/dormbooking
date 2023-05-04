6edc406a6145be51488a02ae6a71b6d5b3617eff
moepyaePK <6531503158@lamduan.mfu.ac.th>
2023-05-04 22:20:15 +0700

    trialllllllllll

--- Sakhtong12Info.java
+++ Sakhtong12Info.java
@@ -1,7 +1,7 @@
 
 public class Sakhtong12Info extends GeneralDorm{
    public static int[][] rooms = new int[40][2];
-	 private double price = 4000;
+	 private double price = 6000;
 	 private String bill = "Pay per unit";
         
    public Sakhtong12Info(String name){
